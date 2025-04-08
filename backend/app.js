f0157802384e2eea5c730f61a5c511d6ce86dc25
Vansh Singhal <vansh.singhal@hotmail.com>
2025-04-08 11:41:26 +0530

    minor change

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
     origin: "http://localhost:5173",
-    credentials: true
 }
 app.use(cors(corsOption));
 
