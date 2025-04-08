7df7ccb88dcdf5f7eb5148776e14dde6d3d2d4fd
Vansh Singhal <vansh.singhal@hotmail.com>
2025-04-08 13:59:30 +0530

    prod ready v-0.6

--- frontend/src/utils/constant.jsx
+++ frontend/src/utils/constant.jsx
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "http://localhost:8000/api/user";
-export const ADMIN_API_ENDPOINT = "http://localhost:8000/api/admin";
-export const TRANSACTION_API_ENDPOINT = "http://localhost:8000/api/transaction";
-export const REVERSAL_API_ENDPOINT = "http://localhost:8000/api/reversal";
+export const USER_API_ENDPOINT = "https://sit-coin.onrender.com/api/user";
+export const ADMIN_API_ENDPOINT = "https://sit-coin.onrender.com/api/admin";
+export const TRANSACTION_API_ENDPOINT = "https://sit-coin.onrender.com/api/transaction";
+export const REVERSAL_API_ENDPOINT = "https://sit-coin.onrender.com/api/reversal";
