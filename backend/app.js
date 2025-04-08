00f79e544f577630829b054360d296fe5094a73e
Vansh Singhal <vansh.singhal@hotmail.com>
2025-04-08 11:37:16 +0530

    prod ready

--- backend/app.js
+++ backend/app.js
@@ -15,21 +15,28 @@ const dbgr = debug("development:app");
 dotenv.config();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
-    origin : "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true
 }
 app.use(cors(corsOption));
 
-app.use("/api/user",userRouter);
-app.use("/api/transaction",transactionRouter);
-app.use("/api/reversal",reversalRouter);
-app.use("/api/admin",adminRouter);
+app.use("/api/user", userRouter);
+app.use("/api/transaction", transactionRouter);
+app.use("/api/reversal", reversalRouter);
+app.use("/api/admin", adminRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     dbgr(`Server started at port ${PORT}`);
     connectdb();
 });
