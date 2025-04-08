619a63a787b4d7a55cf949d6b0fa2598b878e92d
Vansh Singhal <vansh.singhal@hotmail.com>
2025-04-08 13:48:17 +0530

    prod ready v-0.5

--- backend/app.js
+++ backend/app.js
@@ -30,9 +30,9 @@ app.use("/api/reversal", reversalRouter);
 app.use("/api/admin", adminRouter);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(_dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
     })
 }
 
